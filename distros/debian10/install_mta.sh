b3147a1b5a6920e478f92dd685281b6f0b399338
MWohlgemuth <wohlgemuth@schildhain.de>
2020-01-12 22:02:06 +0100

    readded sieve and antispam plugin for dovecot as well as opendkum and opendmarc

--- distros/debian10/install_mta.sh
+++ distros/debian10/install_mta.sh
@@ -3,7 +3,8 @@
 #    Install chosen MTA. Courier or Dovecot
 #---------------------------------------------------------------------
 InstallMTA() {
+  CFG_MTA='dovecot'
   echo -n "Installing POP3/IMAP Mail server (Dovecot) and Mail signing (OpenDKIM, OpenDMARC)... ";
-  apt_install dovecot-imapd dovecot-pop3d dovecot-mysql dovecot-sieve dovecot-lmtpd sudo 
+  apt_install dovecot-imapd dovecot-pop3d dovecot-mysql dovecot-sieve dovecot-lmtpd sudo dovecot-managesieved dovecot-sieve dovecot-antispam opendkim opendkim-tools opendmarc
   echo -e "[${green}DONE${NC}]\n"
 }
